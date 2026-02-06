bb338035263515ccbd6db41105a53c98ae2e64c2
Mitchel Wassler <mpwassler@users.noreply.github.com>
2026-02-06 13:54:48 -0500

    Add vitest Three.js stubs

---
+++ web/src/test/three-orbit-controls-stub.js
@@ -0,0 +1,10 @@
+export class OrbitControls {
+    constructor() {
+        this.enabled = false;
+        this.dampingFactor = 0;
+        this.target = { set() {} };
+    }
+
+    update() {}
+    addEventListener() {}
+}
